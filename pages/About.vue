0391e338f90f18e37b199e93df51f57c78980818
aseggelke <aaron.seggelke@kernpunkt.de>
2021-05-08 14:15:52 +0200

    Init Nuxt.js project

---
+++ pages/About.vue
@@ -0,0 +1,14 @@
+<template>
+  <GridWidth>
+    About Page
+  </GridWidth>
+</template>
+
+<script>
+import GridWidth from "~/components/grid_width/GridWidth";
+
+export default {
+  name: "About",
+  components: {GridWidth},
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <GridWidth>
+    <TestComponent />
+  </GridWidth>
+</template>
+
+<script>
+import TestComponent from "~/components/TestComponent";
+import GridWidth from "~/components/grid_width/GridWidth";
+
+export default {
+  name: 'Startseite',
+  components: {TestComponent, GridWidth},
+}
+</script>
